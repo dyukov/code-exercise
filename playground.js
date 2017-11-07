6c2b6ef59d6e825f7831e6eeaf4e8cf93def832e
Andreas <urbanski.andreas@gmail.com>
2017-11-07 18:40:21 +0200

    Add playground

---
+++ playground.js
@@ -0,0 +1,32 @@
+const { client, feed } = require('./src');
+
+// Let's do something cool with observable changefeeds!
+// Use your imagination
+client().then(r => {
+
+  feed(r.table('test'))
+  .filter(change => change.type === 'insert')
+  .map(change => change.next)
+  .subscribe(item => {
+    console.log('this item was inserted: ', item);
+  });
+
+  feed(r.table('test'))
+  .filter(change => change.type === 'update')
+  .map(change => [change.prev, change.next])
+  .subscribe(([prev, next]) => {
+    console.log('this item was updated: ', prev, 'with new value: ', next);
+  });
+
+  setInterval(() => {
+    r.table('test').insert({value: 'foo'});
+  }, 2000);
+
+  setInterval(() => {
+    r.table('test').filter({value: 'foo'}).limit(1).update({value: 'bar'});
+  }, 2000);
+
+})
+.catch(e => {
+  console.log('oops', e);
+});
