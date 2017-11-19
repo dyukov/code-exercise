efa95082aa85bc20d312cb601e47819fbca0ca72
dyukov <a.dyukov@hotmail.com>
2017-11-19 18:24:48 +0200

    read delete tests for client

--- test/client.test.js
+++ test/client.test.js
@@ -7,12 +7,41 @@ describe('#client', () => {
 
   before(async () => {
     r = await client();
+    await r.dbCreate('ClientTestTablesDb');
   });
 
-  // TODO: Add test cases
-  //
+  // WORKING WITH DATABASES
+  it('should create table', (done) => {
+    r.dbCreate('ClientTestDb').then((change) => {
+      expect(change.dbs_created).to.equal(1);
+      done()
+    })
+  })
+
+  it('should drop table', (done) => {
+    r.dbDrop('ClientTestDb').then((change) => {
+      expect(change.dbs_dropped).to.equal(1);
+      done()
+    })
+  })
+
+// TEST WORKING WITH TABLES
+  it('should create table', (done) => {
+    r.db('ClientTestTablesDb').tableCreate('ClientTestTable').then((change) => {
+      expect(change.tables_created).to.equal(1);
+      done()
+    })
+  })
+
+  it('should delete table', (done) => {
+    r.db('ClientTestTablesDb').tableDrop('ClientTestTable').then((change) => {
+      expect(change.tables_dropped).to.equal(1);
+      done()
+    })
+  })
 
   after(async () => {
-    await r.getPoolMaster().drain();
+    await r.dbDrop('ClientTestTablesDb')
+    await r.getPoolMaster().drain()
   });
 });
