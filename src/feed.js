652d0e1ab051937175a8ac083f3b579cdfe4c8aa
dyukov <a.dyukov@hotmail.com>
2017-11-19 18:26:37 +0200

    feed module cleanup

--- src/feed.js
+++ src/feed.js
@@ -5,31 +5,22 @@ const EventEmitter = require('events').EventEmitter;
 // RX observable
 module.exports = (queryObject) => {
 
-// TODO: Make use of RxNode
-// const RxNode = require('rx-node');
-//var subscription = RxNode.fromReadLineStream(queryObject.changes({includeInitial: true, includeStates: true, includeTypes: true}), 'data')
+  // TODO: Make use of RxNode
+  // const RxNode = require('rx-node');
+  //var subscription = RxNode.fromReadLineStream(queryObject.changes({includeInitial: true, includeStates: true, includeTypes: true}), 'data')
 
-const eventEmitter = new EventEmitter()
-const observable = Rx.Observable.fromEvent(eventEmitter, 'data')
+  const eventEmitter = new EventEmitter()
+  const observable = Rx.Observable.fromEvent(eventEmitter, 'data')
 
-//  message.old_val, next: message.new_val
-// [prev, next]
+  queryObject.changes({includeInitial: true, includeTypes: true}).run().then(cursor => {  
+    cursor.on('data', message => {
+      eventEmitter.emit('data', message)
+    }),
+    cursor.on('error', err => {
+      console.log("Error:")
+      console.log(err)
+    })
+  });
 
-// type: 'add'
-// type: 'change'
-
-// queryObject.changes({includeInitial: true, includeTypes: true}).run().then(function(cursor){
-queryObject.changes({includeInitial: true, includeTypes: true}).run().then(function(cursor){  
-  cursor.on('data', function(message) {
-
-    console.log("from me : " + message.type + ". New " + (message.new_val == null ? "NULL" : message.new_val.value) + ". Old " + (message.old_val == null ? "NULL" : message.old_val.value))
-    eventEmitter.emit('data', message)
-  }),
-  cursor.on('error', function(message) {
-     // TODO : Log error - do not emit up the stack
-     // eventEmitter.emit("error", message)
-  })
-});
-
-return observable;
+  return observable;
 };
